ef9c1ddd0d3026866cdd4feeba755f2e675796ae
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-03-26 20:33:52 -0300

    refactor: remove main from CI tests

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "jest"
 
 const config: Config = {
 	collectCoverage: true,
-	collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+	collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
 	coverageDirectory: "coverage",
 	coverageProvider: "v8",
 	testEnvironment: "node",
