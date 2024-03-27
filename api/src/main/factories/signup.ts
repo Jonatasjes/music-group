422864c7717e4f7deb4cf233e5fdf2095d7dbbb8
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-03-26 22:52:21 -0300

    refactor: move sut creation to a factory method

--- api/src/main/factories/signup.ts
+++ api/src/main/factories/signup.ts
@@ -12,6 +12,6 @@ export const makeSignupController = (): Controller => {
 	const bcryptAdapter = new BcryptAdapter(salt)
 	const accountMongoRepository = new AccountMongoRepository()
 	const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-	const signupController = new SignUpController(emailValidator, dbAddAccount)
-	return new LogControllerDecorator(signupController)
+	return new SignUpController(emailValidator, dbAddAccount)
+	// return new LogControllerDecorator(signupController)
 }
