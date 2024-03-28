c416892a46d54a862cb49eda4bbfe3dd8dc4d21b
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-03-27 21:36:21 -0300

    refactor: remove duplicated code

--- api/src/main/decorators/log.spec.ts
+++ api/src/main/decorators/log.spec.ts
@@ -36,6 +36,15 @@ const makeLogErrorRepository = (): LogErrorRepository => {
 	return new LogErrorRepositoryStub()
 }
 
+const makeFakeRequest = (): HttpRequest => ({
+	body: {
+		name: "any_name",
+		email: "any_email@mail.com",
+		password: "any_password",
+		passwordConfirmation: "any_password"
+	}
+})
+
 const makeSut = (): SutTypes => {
 	const controllerStub = makeController()
 	const logErrorRepositoryStub = makeLogErrorRepository()
@@ -51,28 +60,14 @@ describe("Log Controller Decorator", () => {
 	test("Should call Controller handle", async () => {
 		const { sut, controllerStub } = makeSut()
 		const handleSpy = jest.spyOn(controllerStub, "handle")
-		const httpRequest = {
-			body: {
-				name: "any_name",
-				email: "any_email@mail.com",
-				password: "any_password",
-				passwordConfirmation: "any_password"
-			}
-		}
+		const httpRequest = makeFakeRequest()
 		await sut.handle(httpRequest)
 		expect(handleSpy).toHaveBeenCalledWith(httpRequest)
 	})
 
 	test("Should return the same result of Controller handle", async () => {
 		const { sut } = makeSut()
-		const httpRequest = {
-			body: {
-				name: "any_name",
-				email: "any_email@mail.com",
-				password: "any_password",
-				passwordConfirmation: "any_password"
-			}
-		}
+		const httpRequest = makeFakeRequest()
 		const httpResponse = await sut.handle(httpRequest)
 		expect(httpResponse).toEqual({
 			statusCode: 200,
@@ -92,14 +87,7 @@ describe("Log Controller Decorator", () => {
 		const error = serverError(fakeError)
 		const logSpy = jest.spyOn(logErrorRepositoryStub, "log")
 		jest.spyOn(controllerStub, "handle").mockReturnValueOnce(new Promise((resolve) => resolve(error)))
-		const httpRequest = {
-			body: {
-				name: "any_name",
-				email: "any_email@mail.com",
-				password: "any_password",
-				passwordConfirmation: "any_password"
-			}
-		}
+		const httpRequest = makeFakeRequest()
 		await sut.handle(httpRequest)
 		expect(logSpy).toHaveBeenCalledWith("any_stack")
 	})
