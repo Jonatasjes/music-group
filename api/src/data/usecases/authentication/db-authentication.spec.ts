436ef0861b2ec00a58fefebbbf7819b8fa5e57c4
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-06 02:01:08 -0300

    test: ensure DbAuthentication throw if TokenGenerator throws

--- api/src/data/usecases/authentication/db-authentication.spec.ts
+++ api/src/data/usecases/authentication/db-authentication.spec.ts
@@ -59,11 +59,7 @@ const makeSut = (): SutTypes => {
 	const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepositoryStub()
 	const hashComparerStub = makeHashComparerStub()
 	const tokenGeneratorStub = makeTokenGeneratorStub()
-	const sut = new DbAuthentication(
-		loadAccountByEmailRepositoryStub,
-		hashComparerStub,
-		tokenGeneratorStub
-	)
+	const sut = new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub, tokenGeneratorStub)
 	return {
 		sut,
 		loadAccountByEmailRepositoryStub,
@@ -114,9 +110,7 @@ describe("DbAuthentication Usecase", () => {
 
 	test("Should return null if HashComparer returns false", async () => {
 		const { sut, hashComparerStub } = makeSut()
-		jest
-			.spyOn(hashComparerStub, "compare")
-			.mockReturnValueOnce(new Promise((resolve) => resolve(false)))
+		jest.spyOn(hashComparerStub, "compare").mockReturnValueOnce(new Promise((resolve) => resolve(false)))
 		const accessToken = await sut.auth(makeFakeAuthentication())
 		expect(accessToken).toBeNull()
 	})
@@ -126,5 +120,14 @@ describe("DbAuthentication Usecase", () => {
 		const generateSpy = jest.spyOn(tokenGeneratorStub, "generate")
 		await sut.auth(makeFakeAuthentication())
 		expect(generateSpy).toHaveBeenCalledWith("any_id")
+	})
+
+	test("Should throw if TokenGenerator throws", async () => {
+		const { sut, tokenGeneratorStub } = makeSut()
+		jest
+			.spyOn(tokenGeneratorStub, "generate")
+			.mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+		const promise = sut.auth(makeFakeAuthentication())
+		await expect(promise).rejects.toThrow()
 	})
 })
