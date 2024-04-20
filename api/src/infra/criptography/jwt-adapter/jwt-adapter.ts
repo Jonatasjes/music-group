9201d50bc5783520459f6df8d548c7daebc2d465
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-20 20:45:28 -0300

    feat: ensure JwtAdapter returns a token when sign succeds

---
+++ api/src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+import { Encrypter } from "../../../data/protocols/criptography/encrypter"
+
+export class JwtAdapter implements Encrypter {
+	private readonly secret: string
+	constructor(secret: string) {
+		this.secret = secret
+	}
+	async encrypt(id: string): Promise<string> {
+		const accessToken = await jwt.sign({ id }, this.secret)
+		return accessToken
+	}
+}
