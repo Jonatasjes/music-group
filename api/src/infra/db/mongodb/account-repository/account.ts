6a77d16e44ed06bdbe6311b566c06aa0a5bdebf3
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-20 21:56:55 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- api/src/infra/db/mongodb/account-repository/account.ts
+++ api/src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,24 @@
 import { AddAccountRepository } from "../../../../data/protocols/db/add-account-repository"
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load_account-by-email-repository"
 import { AccountModel } from "../../../../domain/models/account"
 import { AddAccountModel } from "../../../../domain/usecases/add-account"
 import { MongoHelper } from "../helpers/mongo-helper"
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
 	async add(accountData: AddAccountModel): Promise<AccountModel> {
 		const accountCollection = await MongoHelper.getCollection("accounts")
 		const result = await accountCollection.insertOne(accountData)
 		return MongoHelper.map(result, accountData)
+	}
+
+	async loadByEmail(email: string): Promise<AccountModel> {
+		const accountCollection = await MongoHelper.getCollection("accounts")
+		const account = await accountCollection.findOne({ email })
+
+		if (account) {
+			const { _id, name, password } = account
+			return account && Object.assign({}, { name, password, email: account.email }, { id: _id.toString() })
+		}
+		return null
 	}
 }
