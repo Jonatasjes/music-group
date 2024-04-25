ca28a5ff520880e8245d1146e6ae82f988cdb013
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-24 21:15:16 -0300

    refactor: separete factories in folders

--- api/src/main/config/routes.ts
+++ api/src/main/config/routes.ts
@@ -1,12 +1,8 @@
 import { Express, Router } from "express"
-import { readdirSync } from "fs"
+import loginRoutes from "../routes/login-routes"
 
 export default (app: Express): void => {
 	const router = Router()
 	app.use("/api", router)
-	readdirSync(`${__dirname}/../routes`).map(async (file) => {
-		if (!file.includes(".test.")) {
-			(await import(`../routes/${file}`)).default(router)
-		}
-	})
+	loginRoutes(router)
 }
