a85badec82d269cdd01e0877bf2d51dc0aa55ce1
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-27 20:35:38 -0300

    test: ensure PlayerMongoRepository add a new player on add success

---
+++ api/src/infra/db/mongodb/player/player-mongo-repository.spec.ts
@@ -0,0 +1,37 @@
+import { Collection } from "mongodb"
+import { MongoHelper } from "../helpers/mongo-helper"
+import { PlayerMongoRepository } from "./player-mongo-repository"
+import { AddPlayerModel } from "../../../../domain/usecases/add-player"
+
+const makeAddPlayerModel = (): AddPlayerModel => ({
+	name: "any_name",
+	instrument: ["any_instrument"]
+})
+
+let playerCollection: Collection
+
+describe("Player Mongo Repository", () => {
+	beforeAll(async () => {
+		await MongoHelper.connect(process.env.MONGO_URL)
+	})
+
+	afterAll(async () => {
+		await MongoHelper.disconnect()
+	})
+
+	beforeEach(async () => {
+		playerCollection = await MongoHelper.getCollection("players")
+		await playerCollection.deleteMany({})
+	})
+
+	const makeSut = (): PlayerMongoRepository => {
+		return new PlayerMongoRepository()
+	}
+
+	test("Should add a player on success", async () => {
+		const sut = makeSut()
+		await sut.add(makeAddPlayerModel())
+		const player = await playerCollection.findOne({ name: "any_name" })
+		expect(player).toBeTruthy()
+	})
+})
