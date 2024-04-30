e0bd03d4b1a027d46204d996caf96f434409410b
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-29 23:55:29 -0300

    feat: ensure AddPlayerRoute returns 403 if no accessToken is provided

--- api/src/main/routes/player-routes.ts
+++ api/src/main/routes/player-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
 import { adaptRoute } from "../adapters/express-route-adapter"
 import { makeAddPlayerController } from "../factories/controllers/player/add-player/add-player-controller-factory"
+import { makeAuthMiddleware } from "../factories/middlewares/auth-middleware-factory"
+import { adaptMiddleware } from "../adapters/express-middleware-adapter"
 
 export default (router: Router): void => {
-	router.post("/players", adaptRoute(makeAddPlayerController()))
+	const adminAuth = adaptMiddleware(makeAuthMiddleware("admin"))
+	router.post("/players", adminAuth, adaptRoute(makeAddPlayerController()))
 }
