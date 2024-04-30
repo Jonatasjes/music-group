56fd52d3c0ec2a1401487beb5a988aa5b3e75747
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-04-29 22:27:39 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct velues

--- api/src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
+++ api/src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -1,3 +1,4 @@
 export * from "../../protocols/criptography/decrypter"
+export * from "../../protocols/db/account/load_account-by-token-repository"
 export * from "../../../domain/usecases/load-account-by-token"
 export * from "../../../domain/models/account"
--- api/src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ api/src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,11 +1,23 @@
-import { AccountModel, Decrypter, LoadAccountByToken } from "./db-load-account-by-token-protocols"
+import {
+	AccountModel,
+	Decrypter,
+	LoadAccountByToken,
+	LoadAccountByTokenRepository
+} from "./db-load-account-by-token-protocols"
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
-	constructor(private readonly decrypter: Decrypter) {
+	constructor(
+		private readonly decrypter: Decrypter,
+		private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+	) {
 		this.decrypter = decrypter
+		this.loadAccountByTokenRepository = loadAccountByTokenRepository
 	}
 	async load(accessToken: string, role?: string): Promise<AccountModel> {
-		await this.decrypter.decrypt(accessToken)
+		const token = await this.decrypter.decrypt(accessToken)
+		if (token) {
+			await this.loadAccountByTokenRepository.loadByToken(token, role)
+		}
 		return null
 	}
 }
