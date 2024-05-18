45b03a84b2c63a540d32f9caa2305fe62e27403e
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2024-05-18 17:56:00 -0300

    test: ensure add player route returns 204 if valid access token is provided

--- api/src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ api/src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -74,7 +74,7 @@ describe("DbLoadAccountByToken Usecase", () => {
 		const { sut, loadAccountByTokenRepositoryStub } = makeSut()
 		const loadByTokenSpy = jest.spyOn(loadAccountByTokenRepositoryStub, "loadByToken")
 		await sut.load("any_token", "any_role")
-		expect(loadByTokenSpy).toHaveBeenCalledWith("decrypted_token", "any_role")
+		expect(loadByTokenSpy).toHaveBeenCalledWith("any_token", "any_role")
 	})
 
 	test("Should return null if LoadAccountByTokenRepository returns null", async () => {
--- api/src/main/routes/player-routes.test.ts
+++ api/src/main/routes/player-routes.test.ts
@@ -2,8 +2,11 @@ import request from "supertest"
 import app from "../config/app"
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper"
 import { Collection } from "mongodb"
+import { sign } from "jsonwebtoken"
+import env from "../config/env"
 
 let playerCollection: Collection
+let accountCollection: Collection
 
 describe("Player Routes", () => {
 	beforeAll(async () => {
@@ -17,6 +20,9 @@ describe("Player Routes", () => {
 	beforeEach(async () => {
 		playerCollection = await MongoHelper.getCollection("players")
 		await playerCollection.deleteMany({})
+
+		accountCollection = await MongoHelper.getCollection("accounts")
+		await accountCollection.deleteMany({})
 	})
 
 	describe("POST /players", () => {
@@ -28,6 +34,37 @@ describe("Player Routes", () => {
 					instrument: ["contrabaixo", "teclado", "bateria"]
 				})
 				.expect(403)
+		})
+
+		test("Should return 204 on add player with valid accessToken", async () => {
+			const res = await accountCollection.insertOne({
+				name: "jonatas",
+				email: "jonatas@mail.com",
+				password: "123",
+				role: "admin"
+			})
+			const id = res.insertedId.toString()
+			const accessToken = sign({ id }, env.jwtSecret)
+
+			await accountCollection.updateOne(
+				{
+					_id: res.insertedId
+				},
+				{
+					$set: {
+						accessToken
+					}
+				}
+			)
+
+			await request(app)
+				.post("/api/players")
+				.set("x-access-token", accessToken)
+				.send({
+					name: "Jonatas",
+					instrument: ["contrabaixo", "teclado", "bateria"]
+				})
+				.expect(204)
 		})
 	})
 })
